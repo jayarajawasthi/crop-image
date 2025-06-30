83f46676ad99c94b5edefc2d065de7474ec30759
jayaraj awasthi <awasthijayaraj11@gmail.com>
2025-06-30 11:24:58 +0545

    add drag and drop functionality and update css code

--- src/Components/LeftPanel/Leftpanel.jsx
+++ src/Components/LeftPanel/Leftpanel.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LeftPanel.css";
 
-const LeftPanel = () => {
+const LeftPanel = ({ onImageUpload }) => {
+
+    const [dragOver, setDragOver] = useState(false);
+
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setDragOver(false);
+
+        const file = e.dataTransfer.files[0];
+        if(file && file.type.startsWith("image/")){
+            onImageUpload(file);
+        }
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if(file && file.type.startsWith("image/")) {
+            onImageUpload(file);
+        }
+    }
+
     return(
         <div className="left-panel">
             <h2>Upload and resize</h2>
             <p>
                 Drag and drop your image. we'll crop, resize and show platform specific previews.
             </p>
-            <div className="drop-zone">Drop Image Here</div>
+
+            <div className={`drop-zone ${dragOver ? "drag-over" : ""}`} 
+            onDragOver={(e) => {
+                e.preventDefault();
+                setDragOver(true);
+            }}
+            onDragLeave={()=> setDragOver(false)}
+            onDrop={handleDrop}
+            >
+                Drop image here or 
+                <label className="upload-button">
+                    select <input type="file" accept="image/*" onChange={{handleFileChange}} />
+                </label>
+            </div>
         </div>
 
     )
--- src/Components/RightPanel/RightPanel.jsx
+++ src/Components/RightPanel/RightPanel.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./RightPanel.css";
 
 
-const RightPanel = () => {
+const RightPanel = ({ imageURL }) => {
     return(
         <div className="right-panel">
             <h3>Live Preview</h3>
-            <div className="preview-box">Image Preview Here</div>
+            <div className="preview-box">
+                {imageURL ? (<img src={imageURL} alt="Preview" className="preview-image" />): (
+                    <p> No image selected</p>
+                )}
+            </div>
         </div>
     )
 }
